eb1a3c8b11e6ddd1ee79edcf40518a2f57e3ef37
Juan-Cinseros-Amengual <9405569@alumnos.ufv.es>
2026-04-08 20:12:43 +0200

    Fase 2

--- banco/C/banco.c
+++ banco/C/banco.c
@@ -5,6 +5,9 @@
 #include <sys/msg.h>
 #include "estructuras.h"
 #include "config.h"
+#include <fcntl.h>           // Para las constantes O_CREAT
+#include <sys/stat.h>        // Para las constantes de permisos (0644)
+#include <semaphore.h>       // Para usar los semáforos POSIX
 
 int msgid;
 
@@ -53,16 +56,28 @@ void bucle_principal() {
 }
 
 int main() {
-    // 1. Cargar la configuración ANTES de hacer nada más
+    // 1. Cargar la configuración
     cargar_configuracion("../C/config.txt");
+    printf("[SISTEMA] Configuracion cargada exitosamente.\n");
 
-    // [TEST] Comprobamos que ha leído bien imprimiendo un par de variables
-    printf("[TEST] Proximo ID a asignar: %d\n", config_banco.proximo_id);
-    printf("[TEST] Limite de transferencias EUR: %.2f\n", config_banco.lim_trf_eur);
-    printf("[TEST] Umbral de retiros sospechosos: %d\n", config_banco.umbral_retiros);
-    printf("--------------------------------------------------\n");
+    // --- NUEVO: FASE 2 (SEMÁFOROS) ---
+    // Truco pro: Desvinculamos los semáforos antes de crearlos por si 
+    // el programa se cerró mal en una ejecución anterior y se quedaron bloqueados[cite: 250, 252].
+    sem_unlink("/sem_cuentas");
+    sem_unlink("/sem_config");
 
-    // 2. Crear cola de mensajes (código original del esqueleto)
+    // Creamos los semáforos inicializados a 1 (verde)
+    sem_t *sem_cuentas = sem_open("/sem_cuentas", O_CREAT, 0644, 1);
+    sem_t *sem_config = sem_open("/sem_config", O_CREAT, 0644, 1);
+
+    if (sem_cuentas == SEM_FAILED || sem_config == SEM_FAILED) {
+        perror("Error critico creando semaforos");
+        exit(1);
+    }
+    printf("[SISTEMA] Semaforos inicializados correctamente.\n");
+    // ---------------------------------
+
+    // 2. Crear cola de mensajes
     msgid = msgget(IPC_PRIVATE, 0666 | IPC_CREAT);
     if (msgid < 0) {
         perror("Error creando cola de mensajes");
@@ -71,6 +86,13 @@ int main() {
 
     lanzar_monitor();
     bucle_principal();
+
+    // Nota: Aunque por ahora el bucle_principal es infinito y no llega aquí,
+    // es buena práctica cerrarlos[cite: 250, 252].
+    sem_close(sem_cuentas);
+    sem_close(sem_config);
+    sem_unlink("/sem_cuentas");
+    sem_unlink("/sem_config");
 
     return 0;
 }
