5448298a1f121a72e7821f70f210f27270dbd696
JJ0132 <9406490@alumnos.ufv.es>
2026-04-15 13:24:48 +0200

    Fase 4 finalizada

--- banco/C/usuario.c
+++ banco/C/usuario.c
@@ -4,6 +4,11 @@
 #include <unistd.h>
 #include "estructuras.h"
 
+// --- NUEVO: Variables globales para la Fase 4 ---
+int numero_cuenta;
+int pipe_lectura;
+// ------------------------------------------------
+
 void *ejecutar_operacion(void *arg) {
 
     // TODO: implementar operación bancaria
@@ -38,24 +43,44 @@ void procesar_opcion(int opcion) {
             break;
 
         case 6:
-            exit(0);
+            // Quita el exit(0) de aquí para dejar que el main cierre las cosas bien
+            printf("[USUARIO] Cerrando sesion de la cuenta %d...\n", numero_cuenta);
+            break;
 
         default:
             printf("Opcion invalida\n");
     }
 }
 
+// --- NUEVO: El main actualizado ---
 int main(int argc, char *argv[]) {
 
-    int opcion;
+    // 1. Verificamos que el banco nos llama correctamente con 3 argumentos
+    if (argc != 3) {
+        fprintf(stderr, "Uso incorrecto. Debe llamarse desde el banco.\n");
+        exit(1);
+    }
 
-    while (1) {
+    // 2. Guardamos los argumentos (textos) convirtiéndolos a números (atoi)
+    numero_cuenta = atoi(argv[1]);
+    pipe_lectura = atoi(argv[2]);
+
+    printf("\n[USUARIO] Sesion iniciada para la cuenta %d\n", numero_cuenta);
+    printf("[USUARIO] Escuchando alertas del banco en el pipe %d\n", pipe_lectura);
+
+    int opcion = 0;
+
+    // Tu bucle original, pero con una condición de salida limpia
+    while (opcion != 6) {
 
         mostrar_menu();
+        printf("Opcion: ");
         scanf("%d", &opcion);
 
         procesar_opcion(opcion);
     }
 
+    // Cerramos el pipe antes de salir del programa
+    close(pipe_lectura);
     return 0;
 }
